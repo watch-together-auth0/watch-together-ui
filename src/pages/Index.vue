48686698ba68973efc15987679292b03a940c717
kaushikprasanth <kaushikprasanth94@yahoo.com>
2020-08-08 01:36:20 -0500

    Added Family Image Popular Videos in MainPage

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,10 +1,32 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
-    >
-  </q-page>
+ <div class="col">
+  <div class="row" >
+   <div class="col col-md-2">
+        <q-img src="logo.png" style="max-width: 250px">
+        </q-img>
+      </div>
+      <div class="col">
+        <h5 class = "text-purple-wt text-weight-bold top-margin" >Popular Videos</h5>
+         <div class="q-pa-md">
+            <div class="row">
+              <div class="col">  <q-video class="q-pa-sm"src="https://www.youtube.com/embed/k3_tw44QsZQ?rel=0" /></div>
+              <div class="col">  <q-video class="q-pa-sm"src="https://www.youtube.com/embed/k3_tw44QsZQ?rel=0" /></div>
+              <div class="col">  <q-video class="q-pa-sm"src="https://www.youtube.com/embed/k3_tw44QsZQ?rel=0" /></div>
+              <div class="col">  <q-video class="q-pa-sm"src="https://www.youtube.com/embed/k3_tw44QsZQ?rel=0" /></div>
+            </div>
+      </div>
+   </div>
+   <div class="row">
+     <div class="col">
+        <q-img src="family.jpg" style="max-height: 500px">
+          <div class="absolute-full text-heading flex flex-center ">
+            <h3 class = "text-purple-wt text-weight-bold">Enjoy YouTube, Vimeo videos and more with your family</h3>
+          </div>
+        </q-img>
+      </div>
+   </div>
+</div>
+ </div>
 </template>
 
 <script>
