31e96526b81f5e0044e00b7f528f709f423138e3
ajcastro <ajcastro29@gmail.com>
2020-08-08 13:03:29 +0800

    Add Sidebar

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,25 +1,61 @@
 <template>
+<div>
   <q-list>
+    <q-item clickable v-ripple @click="$root.$emit('toggleSideBar')">
+      <q-item-section></q-item-section>
+      <q-item-section avatar>
+        <q-icon color="primary" :name="leftActive ? 'chevron_left' : 'chevron_right'" />
+      </q-item-section>
+    </q-item>
     <q-expansion-item
-      expand-separator
-      icon="perm_identity"
-      label="Account settings"
-      caption="John Doe">
-      <q-card>
-        <q-card-section>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
-          commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
-          eveniet doloribus ullam aliquid.
-        </q-card-section>
-      </q-card>
+      icon="people_outline"
+      label="Online">
+      <q-list>
+        <q-separator />
+        <q-item v-for="user in onlineUsers" :key="user.id" clickable v-ripple>
+          <q-item-section avatar>
+            <q-icon name="account_circle" />
+          </q-item-section>
+          <q-item-section>
+            {{ user.name }}
+          </q-item-section>
+        </q-item>
+      </q-list>
     </q-expansion-item>
   </q-list>
+  <q-separator />
+  <q-list>
+    <q-item>
+      <q-item-section>Video Channels</q-item-section>
+      <q-item-section avatar>
+        <q-btn dense flat icon="add_box" />
+      </q-item-section>
+    </q-item>
+    <q-separator />
+    <template v-if="leftActive">
+    <q-item v-for="channel in videoChannels" :key="channel.id" clickable v-ripple>
+      <q-item-section avatar>
+        <q-btn dense flat icon="play_arrow" />
+      </q-item-section>
+      <q-item-section>
+        <q-item-label>{{ channel.name }}</q-item-label>
+      </q-item-section>
+    </q-item>
+    </template>
+  </q-list>
+</div>
 </template>
 
 <script>
 export default {
+  mounted() {
+    this.$root.$on('leftActiveChanged', (bool) => {
+      this.leftActive = bool;
+    });
+  },
   data() {
     return {
+      leftActive: true,
       onlineUsers: [
         {
           id: 1,
@@ -27,10 +63,26 @@ export default {
           photo_url: '',
         }, {
           id: 2,
-          name: 'Uncle Sam',
+          name: 'Uncle Drew',
+          photo_url: '',
+        }, {
+          id: 2,
+          name: 'Uncle Ben',
           photo_url: '',
         },
       ],
+      videoChannels: [
+        {
+          id: 1,
+          name: 'Family Guy',
+        }, {
+          id: 2,
+          name: 'National Geographic',
+        }, {
+          id: 3,
+          name: 'Classic',
+        },
+      ]
     };
   },
 };
