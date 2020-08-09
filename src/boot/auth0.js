b8b3801c6c7ab1d3a2719db1f42f24eef0645663
ajcastro <ajcastro29@gmail.com>
2020-08-09 10:43:54 +0800

    Refactor, move auth0 plugin to boot/auth0.js

---
+++ src/boot/auth0.js
@@ -0,0 +1,19 @@
+import Vue from 'vue';
+import router from 'src/router';
+import { Auth0Plugin } from 'src/auth';
+import { domain, clientId } from '../../auth_config.json';
+
+console.log('Domain', domain, clientId);
+
+// Install the authentication plugin here
+Vue.use(Auth0Plugin, {
+  domain,
+  clientId,
+  onRedirectCallback: (appState) => {
+    router.push(
+      appState && appState.targetUrl
+        ? appState.targetUrl
+        : window.location.pathname,
+    );
+  },
+});
