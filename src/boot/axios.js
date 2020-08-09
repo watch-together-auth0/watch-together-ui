09483c8919c03655c851fcac1cdf78dfd00644d5
ajcastro <ajcastro29@gmail.com>
2020-08-09 23:31:26 +0800

    Setup axios

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,4 +1,10 @@
 import Vue from 'vue';
 import axios from 'axios';
 
+axios.defaults.headers = {};
+axios.defaults.headers['Content-type'] = 'application/json';
+axios.defaults.headers.Accept = 'application/json';
+axios.defaults.baseURL = process.env.API_URL;
+console.log('process.env.API_URL', process.env.API_URL);
+
 Vue.prototype.$axios = axios;
