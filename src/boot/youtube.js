2cf1f92304806af45dceff853f5084f57bccc217
ajcastro <ajcastro29@gmail.com>
2020-08-10 08:09:25 +0800

    Create fallbackVideos()

--- src/boot/youtube.js
+++ src/boot/youtube.js
@@ -13,8 +13,73 @@ class Yt {
     });
   }
 
-  search(q, params = {}) {
-    return this.axios.get('/search', { params: { q, ...params } });
+  fallbackVideos() {
+    const items = [
+      {
+        id: '7lCDEYXw3mM',
+        kind: 'youtube#video',
+        etag: '"UCBpFjp2h75_b92t44sqraUcyu0/iYynQR8AtacsFUwWmrVaw4Smb_Q"',
+        snippet: {
+          publishedAt: '2012-06-20T22:45:24.000Z',
+          channelId: 'UC_x5XG1OV2P6uZZ5FSM9Ttw',
+          title: 'Avengers - Movie Trailer',
+          description: 'Antonio Fuentes speaks to us and takes questions on working with Google APIs and OAuth 2.0.',
+          thumbnails: {
+            default: {
+              url: 'https://i.ytimg.com/vi/6ZfuNTqbHE8/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBYC5R6XVFCqh73cDAbbTszFAvfrw',
+            },
+            medium: {
+              url: 'https://i.ytimg.com/vi/6ZfuNTqbHE8/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBYC5R6XVFCqh73cDAbbTszFAvfrw',
+            },
+            high: {
+              url: 'https://i.ytimg.com/vi/6ZfuNTqbHE8/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBYC5R6XVFCqh73cDAbbTszFAvfrw',
+            },
+          },
+          categoryId: '28',
+        },
+      },
+      {
+        id: '7lCDEYXw3mM',
+        kind: 'youtube#video',
+        etag: '"UCBpFjp2h75_b92t44sqraUcyu0/iYynQR8AtacsFUwWmrVaw4Smb_Q"',
+        snippet: {
+          publishedAt: '2012-06-20T22:45:24.000Z',
+          channelId: 'UC_x5XG1OV2P6uZZ5FSM9Ttw',
+          title: 'Avengers - Movie Trailer',
+          description: 'Antonio Fuentes speaks to us and takes questions on working with Google APIs and OAuth 2.0.',
+          thumbnails: {
+            default: {
+              url: 'https://i.ytimg.com/vi/eOrNdBpGMv8/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBFiFm2p4acvv5OVLI02zOUGAD4eQ',
+            },
+            medium: {
+              url: 'https://i.ytimg.com/vi/eOrNdBpGMv8/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBFiFm2p4acvv5OVLI02zOUGAD4eQ',
+            },
+            high: {
+              url: 'https://i.ytimg.com/vi/eOrNdBpGMv8/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBFiFm2p4acvv5OVLI02zOUGAD4eQ',
+            },
+          },
+          categoryId: '28',
+        },
+      },
+    ];
+    for (let index = 0; index < 7; index += 1) {
+      items.push(items[0]);
+      items.push(items[1]);
+    }
+    return Promise.resolve({
+      data: {
+        items,
+      },
+    });
+  }
+
+  async search(q, params = {}) {
+    try {
+      return await this.axios.get('/search', { params: { q, ...params } });
+    } catch (e) {
+      console.log('Yt -> search -> e', e.response);
+      return this.fallbackVideos();
+    }
   }
 }
 
